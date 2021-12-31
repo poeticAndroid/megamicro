fe1289e8ed61b3805ac9be8d47e6903ad5f11f62
poeticAndroid <i@poeticAndroid.online>
2021-12-31 23:22:47 +0100

    cpu mvp

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello there, world!")
+(() => {
+  let cpu
+  let ram = new WebAssembly.Memory({ initial: 1 })
+
+  async function init() {
+    await loadCPU("cypu.wasm", { pcb: { ram: ram } })
+    render()
+  } init()
+
+  async function loadCPU(path, imports) {
+    let bin = await (await fetch(path)).arrayBuffer()
+    await WebAssembly.instantiate(bin, imports).then(wasm => {
+      cpu = wasm.instance.exports
+    })
+  }
+
+  function render(t) {
+    let opcode = cpu.run(1)
+    // console.log(cpu.getReg(0), opcode)
+    requestAnimationFrame(render)
+  }
+})()
