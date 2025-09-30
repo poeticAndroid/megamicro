a8ad273412e4f5758cab9c7ff7713cdaea8497b5
poeticAndroid <i@poeticAndroid.online>
2025-09-30 10:15:07 +0200

    just a plain ol' static webserver

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const http = require("http"),
 const hostname = "127.0.0.1"
 const port = process.env.PORT
 
-let updated = [],
-  notfound = []
-
 const server = http.createServer((req, res) => {
   res.setHeader("Cache-Control", "max-age=4096")
   let filename = "." + req.url
@@ -19,12 +16,6 @@ const server = http.createServer((req, res) => {
     res.statusCode = 404
     res.setHeader("Content-Type", "text/html; charset=utf-8")
     res.end("<h1> nope ")
-    return
-  }
-  if (notfound.includes(filename)) {
-    res.statusCode = 404
-    res.setHeader("Content-Type", "text/html; charset=utf-8")
-    res.end("<h1> still nothing here ")
     return
   }
   let ext = filename.slice(filename.lastIndexOf("."))
@@ -41,45 +32,12 @@ const server = http.createServer((req, res) => {
     default:
       res.setHeader("Content-Type", "text/plain; charset=utf-8")
   }
+  console.log("Reading", filename)
   fs.readFile(filename, (err, data) => {
-    if (err || !updated.includes(filename)) {
-      fs.mkdirSync(path.dirname(filename), { recursive: true })
-      fs.open(filename, "w", (err, fd) => {
-        if (err) {
-          res.setHeader("Content-Type", "text/html; charset=utf-8")
-          res.end("<h1> could not write file :( ")
-          return
-        }
-        https.get("https://raw.githubusercontent.com/poeticAndroid/megamicro/master/" + filename.slice(2), (resp) => {
-          if (resp.statusCode === 200) {
-            updated.push(filename)
-            resp.on("data", (d) => {
-              fs.writeSync(fd, d, (err) => console.error)
-              res.write(d)
-            })
-            resp.on("end", (err) => {
-              fs.closeSync(fd, (err) => console.error)
-              res.end()
-            })
-          } else {
-            notfound.push(filename)
-            res.statusCode = 404
-            res.setHeader("Content-Type", "text/html; charset=utf-8")
-            res.end("<h1>nothing here")
-            fs.closeSync(fd, (err) => console.error)
-            fs.unlinkSync(filename)
-            filename = path.dirname(filename)
-            while (filename.length > 2) {
-              try {
-                fs.rmdirSync(filename)
-              } catch (error) {
-                //  ¯\_(ツ)_/¯ 
-              }
-              filename = path.dirname(filename)
-            }
-          }
-        })
-      })
+    if (err) {
+      res.statusCode = 404
+      res.setHeader("Content-Type", "text/html; charset=utf-8")
+      res.end("<h1>nothing here")
     } else {
       res.statusCode = 200
       res.end(data)
